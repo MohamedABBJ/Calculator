b18f6179fcb0e9c278c03a2b7a8a9e06e47e4ef3
Yusuf <yusufmaster7@gmail.com>
2020-04-13 16:42:56 -0500

    version 0.3

--- Calculator.js
+++ Calculator.js
@@ -1,10 +1,32 @@
 let storage = "";
+let signtemp
 
-function input(button) {
-    document.getElementById("resultado").innerHTML = document.getElementById("resultado").innerHTML + button.value
-    storage = document.getElementById("resultado").innerHTML
-    storage = storage.replace(/x/g, "*")
-    console.log(storage)
+function input(button,sign) {
+    if(sign){
+        if(signtemp){
+            storage = storage.substring(0, storage.length - 1)
+            storage = storage+button.value
+            document.getElementById("resultado").innerHTML = storage
+        }
+        else{
+            document.getElementById("resultado").innerHTML = document.getElementById("resultado").innerHTML + button.value
+            storage = document.getElementById("resultado").innerHTML
+            storage = fixchar(storage)
+            signtemp = true
+        }
+    }else{
+        document.getElementById("resultado").innerHTML = document.getElementById("resultado").innerHTML + button.value
+        storage = document.getElementById("resultado").innerHTML
+        storage = fixchar(storage)
+        signtemp = false
+    }
+
+}
+
+function fixchar(i){
+    i = i.replace(/x/g, "*")
+    i = i.replace(/÷/g, "/")
+    return i
 }
 
 function result() {
@@ -15,4 +37,3 @@ function del() {
     storage = storage.substring(0, storage.length - 1)
     document.getElementById("resultado").innerHTML = storage
 }
-
